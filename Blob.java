24f7d8d55325485b630af565e68fc1f5ffc6f8d3
Hugh Amshen <hamshen1@hwemail.com>
2024-09-26 14:48:33 -0700

    Fixed recursive delete, worked on fixing blob directory implement
    Blob implement not tested but started tester

--- Blob.java
+++ Blob.java
@@ -9,43 +9,66 @@ import java.security.NoSuchAlgorithmException;
 import java.math.BigInteger;
 
 public class Blob {
-    public File file;
+    public File newFile;
+    public File ogFile;
     public String data;
     public String hashedName;
     public boolean isDirectory;
-    public String objPath;
+    public String newPath;
+    public String newPathFormatted;
+    public String ogPath;
+
     // potentially need to initialize it as data = ""??
 
-    public Blob(String fileName) throws IOException, NoSuchAlgorithmException {
-        file = new File(fileName);
-        isDirectory = file.isDirectory();
-        data = copyData();
+    public Blob(Path filePath) throws IOException, NoSuchAlgorithmException {
+        ogFile = new File(filePath.toString());
+        newFile= new File("./git/objects" + filePath);
+        newPath = newFile.getPath();
+        newPathFormatted = newPath.substring(13);
+        isDirectory = ogFile.isDirectory();
+
+        newFile.createNewFile();
+
+        data = copyData(ogPath);
         hashedName = convertToSha(data);
         
-
         if(isDirectory) {
-            Files.write(Paths.get("./git/index"), ("tree " + hashedName + " " + fileName + '\n').getBytes(), StandardOpenOption.APPEND);
+            Files.write(Paths.get("./git/index"), ("tree " + hashedName + " " + filePath + '\n').getBytes(), StandardOpenOption.APPEND);
         }
         else {
-            Files.write(Paths.get("./git/index"), ("blob " + hashedName + " " + fileName + '\n').getBytes(), StandardOpenOption.APPEND);
+            Files.write(Paths.get("./git/index"), ("blob " + hashedName + " " + filePath + '\n').getBytes(), StandardOpenOption.APPEND);
         }
     }
 
-    public String copyData() throws IOException {
-        Path path = file.toPath();
-        return new String(Files.readAllBytes(path));
+    public String copyData(String path) throws IOException {
+        return new String(Files.readAllBytes(Paths.get(path)));
     }
 
     public String convertToSha(String dataInFile) throws NoSuchAlgorithmException, IOException {
-        if(file.isDirectory()) {
-            for(File f : file.listFiles()) {
-                Blob b = new Blob(file.getName() + f.getName());
+
+        String tempData = dataInFile;
+
+        if(ogFile.isDirectory()) {
+            File[] dirList = ogFile.listFiles();
+            
+            for(int i = 0; i < dirList.length; i++) {
+                Blob b = new Blob(Paths.get(ogPath + dirList[i].getName()));
+                
+                if(isDirectory) {
+                    Files.write(Paths.get(newPath), ("tree " + b.getHashedName() + " " + b.getPath() + '\n').getBytes(), StandardOpenOption.APPEND);
+                }
+                else {
+                    Files.write(Paths.get(newPath), ("blob " + b.getHashedName() + " " + b.getPath() + '\n').getBytes(), StandardOpenOption.APPEND);
+                }
             }
+
+            tempData = copyData(newPath);
+
         }
         
         MessageDigest crypt = MessageDigest.getInstance("SHA-1");
         crypt.reset();
-        crypt.update(dataInFile.getBytes("UTF-8"));
+        crypt.update(tempData.getBytes("UTF-8"));
         return new BigInteger(1, crypt.digest()).toString(16);
     }
     // NOT PRINTING CORRECTLY
@@ -56,5 +79,14 @@ public class Blob {
 
     public String getHashedName() {
         return hashedName;
+    }
+
+    public String getPath() {
+        return newPath;
+    }
+
+
+    public boolean isDirectory() {
+        return isDirectory;
     }
 }
---
+++ Tester.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+
+public class Tester {
+    public static void main(String[] args) throws IOException {
+
+        
+        Git.initGitRepo();
+
+        Git.checkInitGitRepo();
+
+    }
+}

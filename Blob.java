cd8593a2fdce0493d0e1cc4d21ba40605cbfce04
Hugh Amshen <hamshen1@hwemail.com>
2024-09-26 13:19:44 -0700

    Added Directory functionality

--- Blob.java
+++ Blob.java
@@ -1,7 +1,8 @@
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.io.File;
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
 import java.nio.file.Files;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
@@ -11,12 +12,23 @@ public class Blob {
     public File file;
     public String data;
     public String hashedName;
+    public boolean isDirectory;
+    public String objPath;
     // potentially need to initialize it as data = ""??
 
     public Blob(String fileName) throws IOException, NoSuchAlgorithmException {
         file = new File(fileName);
+        isDirectory = file.isDirectory();
         data = copyData();
         hashedName = convertToSha(data);
+        
+
+        if(isDirectory) {
+            Files.write(Paths.get("./git/index"), ("tree " + hashedName + " " + fileName + '\n').getBytes(), StandardOpenOption.APPEND);
+        }
+        else {
+            Files.write(Paths.get("./git/index"), ("blob " + hashedName + " " + fileName + '\n').getBytes(), StandardOpenOption.APPEND);
+        }
     }
 
     public String copyData() throws IOException {
@@ -24,7 +36,13 @@ public class Blob {
         return new String(Files.readAllBytes(path));
     }
 
-    public String convertToSha(String dataInFile) throws NoSuchAlgorithmException, UnsupportedEncodingException {
+    public String convertToSha(String dataInFile) throws NoSuchAlgorithmException, IOException {
+        if(file.isDirectory()) {
+            for(File f : file.listFiles()) {
+                Blob b = new Blob(file.getName() + f.getName());
+            }
+        }
+        
         MessageDigest crypt = MessageDigest.getInstance("SHA-1");
         crypt.reset();
         crypt.update(dataInFile.getBytes("UTF-8"));

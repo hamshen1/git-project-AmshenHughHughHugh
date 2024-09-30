c9773480197df02c109476c94e6f7bbe81a0c48e
Hugh Amshen <hamshen1@hwemail.com>
2024-09-30 00:23:16 -0700

    Fixed all of the old, code and tested. Can make directory blobs

--- Blob.java
+++ Blob.java
@@ -3,45 +3,123 @@ import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.io.File;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.List;
 import java.math.BigInteger;
 
 public class Blob {
     public File newFile;
     public File ogFile;
+    public File temp;
     public String data;
     public String hashedName;
+    public String tempPath;
     public boolean isDirectory;
     public String newPath;
-    public String newPathFormatted;
     public String ogPath;
 
     // potentially need to initialize it as data = ""??
 
     public Blob(Path filePath) throws IOException, NoSuchAlgorithmException {
-        ogFile = new File(filePath.toString());
-        newFile= new File("./git/objects" + filePath);
-        newPath = newFile.getPath();
-        newPathFormatted = newPath.substring(13);
+
+        ogFile = new File(filePath.toString()); 
+        ogPath = filePath.toString(); 
+        
         isDirectory = ogFile.isDirectory();
 
-        newFile.createNewFile();
-
         data = copyData(ogPath);
-        hashedName = convertToSha(data);
-        
+
         if(isDirectory) {
-            Files.write(Paths.get("./git/index"), ("tree " + hashedName + " " + filePath + '\n').getBytes(), StandardOpenOption.APPEND);
+            tempPath = "./git/objects/temp";
+            temp = new File(tempPath);
         }
-        else {
-            Files.write(Paths.get("./git/index"), ("blob " + hashedName + " " + filePath + '\n').getBytes(), StandardOpenOption.APPEND);
+    
+
+        hashedName = convertToSha(data);
+
+        newPath = "./git/objects/" + hashedName;
+
+        newFile = new File(newPath);
+
+        if(!Files.exists(Paths.get(newPath))) {
+            if(isDirectory()) {
+                temp.renameTo(newFile);
+            }
+            
+            else {
+                newFile.createNewFile();
+                Files.write(Paths.get(newPath), data.getBytes(), StandardOpenOption.WRITE);
+            }
+            
         }
+        
+        indexWrite("./git/index", this);
+
+        indexDirectory("");
+
     }
 
     public String copyData(String path) throws IOException {
+        if(isDirectory()) {
+            return "";
+        }
         return new String(Files.readAllBytes(Paths.get(path)));
+    }
+
+    public static boolean indexFileContains(String indexPath, String hash) throws IOException {
+        File indexFile = new File(indexPath);
+        if(!indexFile.exists() || indexFile.length() == 0) {
+            return false;
+        }
+
+        List<String> indText = Files.readAllLines(Paths.get(indexFile.getPath()));
+
+        for(String s : indText) {
+            String[] sArr = s.split("\\s+");
+
+            if(sArr[1].equals(hash)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public void indexWrite(String indexPath, Blob blob) throws IOException {
+        if(!indexFileContains(indexPath, blob.getHashedName())) {
+            Path currBlobOgPath = blob.getOgPath();
+            if(blob.isDirectory()) {
+                Files.write(Paths.get(indexPath), ("tree " + blob.getHashedName() + " " + currBlobOgPath.subpath(1, currBlobOgPath.getNameCount()) + '\n').getBytes(), StandardOpenOption.APPEND);
+            }
+            else {
+                Files.write(Paths.get(indexPath), ("blob " + blob.getHashedName() + " " + currBlobOgPath.subpath(1, currBlobOgPath.getNameCount()) + '\n').getBytes(), StandardOpenOption.APPEND);
+            }
+        }
+    }
+
+    public Blob[] blobDirContents() throws NoSuchAlgorithmException, IOException {
+        Blob[] bArr = null;
+        if(isDirectory()) {
+            File[] dirList = ogFile.listFiles();
+            bArr = new Blob[dirList.length];
+
+            
+            if (temp.exists()) {
+                temp.delete();
+            }
+            
+            temp.createNewFile();
+
+            for(int i = 0; i < dirList.length; i++) {
+                Blob b = new Blob(Paths.get(ogPath + "/" + dirList[i].getName()));
+                
+                indexWrite(tempPath, b);
+            }
+        }
+        return bArr;
     }
 
     public String convertToSha(String dataInFile) throws NoSuchAlgorithmException, IOException {
@@ -49,18 +127,8 @@ public class Blob {
         String tempData = dataInFile;
 
         if(ogFile.isDirectory()) {
-            File[] dirList = ogFile.listFiles();
             
-            for(int i = 0; i < dirList.length; i++) {
-                Blob b = new Blob(Paths.get(ogPath + dirList[i].getName()));
-                
-                if(isDirectory) {
-                    Files.write(Paths.get(newPath), ("tree " + b.getHashedName() + " " + b.getPath() + '\n').getBytes(), StandardOpenOption.APPEND);
-                }
-                else {
-                    Files.write(Paths.get(newPath), ("blob " + b.getHashedName() + " " + b.getPath() + '\n').getBytes(), StandardOpenOption.APPEND);
-                }
-            }
+            blobDirContents();
 
             tempData = copyData(newPath);
 
@@ -71,7 +139,32 @@ public class Blob {
         crypt.update(tempData.getBytes("UTF-8"));
         return new BigInteger(1, crypt.digest()).toString(16);
     }
-    // NOT PRINTING CORRECTLY
+    
+    public void indexDirectory(String prefixString) throws IOException {
+        if(isDirectory()) {
+            List<String> dirText = Files.readAllLines(Paths.get(newPath));
+
+            for(String s : dirText) {
+                
+                String[] sArr = s.split("\\s+");
+
+                if (!indexFileContains("./git/index", sArr[1])) {
+    
+                    sArr[2] = prefixString + ogFile.getName() + "/" + sArr[2];
+
+                    String sPrefixed = sArr.toString();
+
+                    Files.write(Paths.get("./git/index"), sPrefixed.getBytes(), StandardOpenOption.APPEND);
+                    
+                }
+
+                if(sArr[0] == "tree") {
+                    indexDirectory(prefixString + ogFile.getName() + "/");
+                }
+            }
+        }
+
+    }
 
     public String getData() {
         return data;
@@ -85,8 +178,12 @@ public class Blob {
         return newPath;
     }
 
-
     public boolean isDirectory() {
         return isDirectory;
     }
+
+    public Path getOgPath() {
+        return Paths.get(ogPath);
+    }
+
 }
